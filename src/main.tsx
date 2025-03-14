be4a86137d16b6e2b979b9abccf0e02666772bd0
S4v!0r <gbp17@outlook.com>
2025-03-14 15:40:33 -0500

    feat: integrar React Query para la obtención de datos en la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TMBDRouter from './router/TMBDRouter.tsx';
 
 import './styles.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TMBDRouter />
+    <QueryClientProvider client={queryClient}>
+      <TMBDRouter />
+    </QueryClientProvider>
   </StrictMode>
 );
