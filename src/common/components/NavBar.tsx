b148fdf2e42dc44e49709b3a1ff0d0a9f2ed6b48
S4v!0r <gbp17@outlook.com>
2025-03-15 19:11:59 -0500

    feat(NavBar): agregar estilos basados en pathname para el enlace de favoritos en NavBar

--- src/common/components/NavBar.tsx
+++ src/common/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { Cat, Film, Heart } from 'lucide-react';
 import { Movie } from '@/movies/interfaces/movieResponse';
 
@@ -8,7 +8,8 @@ interface Props {
 
 export const NavBar = ({ favorites }: Props) => {
   const favoritesCount = favorites.length;
-
+  const { pathname } = useLocation();
+  // console.log(pathname);
   return (
     <header className="sticky top-0 z-10 border-b bg-background/80 backdrop-blur-sm">
       <div className="container flex items-center justify-between h-16 px-4 mx-auto">
@@ -21,7 +22,9 @@ export const NavBar = ({ favorites }: Props) => {
         <div className="flex items-center gap-4">
           <Link
             to={'/favorites'}
-            className="relative inline-flex items-center gap-2 px-3 py-1.5 text-sm font-medium rounded-md hover:bg-secondary transition-colors"
+            className={`relative inline-flex items-center gap-2 px-3 py-1.5 text-sm font-medium rounded-md hover:bg-accent/20 transition-colors ${
+              pathname === '/favorites' ? 'bg-accent/20' : ''
+            }`}
           >
             <Heart className={`w-5 h-5 fill-accent text-accent`} />
             <span className="hidden sm:inline">Favoritos</span>
